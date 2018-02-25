26bf79548b587777330200f0ab293667e2381558
Connor Svrcek <csvrcek@umich.edu>
2018-02-24 23:17:53 -0600

    yeah it worked

---
+++ App.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import AssetExample from './components/AssetWoz';
+import AssetConnor from './components/AssetConnor';
+import { HomeScreen } from './screens'
+import { MainTabNavigator } from './navigation'
+import { Card } from 'react-native-elements'; // 0.19.0
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      console.log('here')
+      return (
+        <View style={{flex: 1}}>
+          <MainTabNavigator/>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.containerBackground}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+          <RootNavigation />
+        </View>
+      );
+    }
+  }
+
+  // _loadResourcesAsync = async () => {
+  //   return Promise.all([
+  //     Asset.loadAsync([
+  //       require('./assets/images/robot-dev.png'),
+  //       require('./assets/images/robot-prod.png'),
+  //     ]),
+  //     Font.loadAsync({
+  //       // This is the font that we are using for our tab bar
+  //       ...Ionicons.font,
+  //       // We include SpaceMono because we use it in HomeScreen.js. Feel free
+  //       // to remove this if you are not using it in your app
+  //       'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+  //     }),
+  //   ]);
+  // };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
+
+// export default class App extends Component<Props> {
+//    render() {
+//      return (
+//        <View style={{flex: 1}}>
+//          <View style={styles.container1}>
+//            <Text style={styles.woz}>
+//              Active Ragers
+//            </Text>
+//            <Card title="Woz's Dream Party">
+//              <AssetExample />
+//            </Card>
+//          </View>
+//
+//          <View style={styles.container2}>
+//            <Card title="Cdids' Wet Party">
+//              <AssetConnor />
+//            </Card>
+//          </View>
+//        </View>
+//      );
+//    }
+//  }
+
+const styles = StyleSheet.create({
+  containerBackground: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  statusBarUnderlay: {
+    height: 24,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+  container1: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF',
+   },
+   container2: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF',
+   },
+   woz: {
+     fontSize: 20,
+     textAlign: 'center',
+     margin: 10,
+   },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+import { MonoText } from '../components/StyledText';
+import AssetConnor from '../components/AssetConnor';
+import AssetWoz from '../components/AssetWoz';
+import AssetJulian from '../components/AssetJulian';
+import { Card } from 'react-native-elements';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return  (
+      <ScrollView style={{flex: 1}}>
+
+         <View style={styles.container}>
+           <Text style={styles.header}>
+             Active Ragers
+           </Text>
+           <Card title="Woz's Dream Party">
+             <AssetWoz />
+           </Card>
+         </View>
+
+         <View style={styles.container}>
+           <Card title="Cdids' Wet Party">
+             <AssetConnor />
+           </Card>
+         </View>
+
+         <View style={styles.container}>
+          <Card title="Jules' hot rager">
+            <AssetJulian />
+          </Card>
+         </View>
+
+       </ScrollView>
+    );
+  }
+
+
+
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  logo: {
+    backgroundColor: "#056ecf",
+    height: 128,
+    width: 128,
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    marginTop: 50,
+  },
+
+
+});
